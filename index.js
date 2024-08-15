599326d78db8b50f70ddb2f0f98ab1d4938bf6cb
Mazed1413 <mamazed1314@gmail.com>
2024-08-15 22:23:15 +0600

    create user related route

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tvtcgta.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // ----------------------------------------------------------------
+    // --------------------DB collection---------------------------
+    // ----------------------------------------------------------------
+
+    const userCollection = client.db("radiantDB").collection("allUser");
+
+    // ----------------------------------------------------------------
+    // --------------------user related route---------------------------
+    // ----------------------------------------------------------------
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await userCollection.findOne({ email });
+      res.send(result);
+    });
+
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      // console.log(email);
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.put("/edit/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateUser = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = { $set: updateUser };
+      const result = await userCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Ser Ser Server is running on port: ${port}`);
+});
