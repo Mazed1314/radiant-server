64c9e4f4ca30fd294c8c885c0ad67461b262ce6d
Mazed1413 <mamazed1314@gmail.com>
2024-08-18 17:08:16 +0600

    add product delete route

--- index.js
+++ index.js
@@ -126,6 +126,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // ----------------------------------------------------------------
     // --------------------rating related route---------------------------
     // ----------------------------------------------------------------
